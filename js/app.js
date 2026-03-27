1f59154e92eacd720d98fc88ddf6ac007b0456a3
lolhackjp <12959114+tryhardcoding@users.noreply.github.com>
2026-03-27 10:51:02 +0900

    Remove PWA install promotion banner

--- js/app.js
+++ js/app.js
@@ -8,12 +8,12 @@ import { initI18n, setLanguage, t, applyTranslations } from './i18n.js';
 import { getTop200Tracks } from './api.js';
 import { getRegion, setRegion, REGIONS, getRegionConfig, getFlagUrl } from './region.js';
 import { getPacksConfig } from './data-loader.js';
-import './gacha.js?v=20260327a'; // ガチャモジュール（グローバル参照にopenPackを登録）
+import './gacha.js?v=20260327b'; // ガチャモジュール（グローバル参照にopenPackを登録）
 import { initCollection, renderCollection, refreshCollection, resetCollectionState } from './collection.js';
 import { initShareHandler } from './transfer.js';
-import { initAds, showRewardedAd, updateAdButton, refreshModalBannerAd } from './ads.js?v=20260327a';
+import { initAds, showRewardedAd, updateAdButton, refreshModalBannerAd } from './ads.js?v=20260327b';
 import { icon, refreshIcons } from './icons.js';
-import { sharePackResult, shareCollectionStats } from './share-sns.js?v=20260327a';
+import { sharePackResult, shareCollectionStats } from './share-sns.js?v=20260327b';
 import { invalidateCache } from './data-loader.js';
 import { checkAchievements, getAchievementStats, renderAchievementModal, trackDailyBonus } from './achievements.js';
 import { getAmazonMusicUnlimitedUrl } from './affiliate.js';
